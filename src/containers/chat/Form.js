a078bdd0f32df063010ddd2ecab711954ec8a278
Thanh Pham <zlatan.pham@gmail.com>
2020-05-04 18:09:05 +0700

    sync source gitlab (#2)

--- src/containers/chat/Form.js
+++ src/containers/chat/Form.js
@@ -1,8 +1,17 @@
-// External
 import React from 'react'
+import { iframe, box, section } from './style.module.scss'
 
-// Internal
-
-const ChatForm = () => <section id="community" />
+const ChatForm = () => (
+  <section className={section}>
+    <div className={box}>
+      <iframe
+        scrolling="no"
+        className={iframe}
+        src="https://chat.webuild.community/"
+        // src="https://daedstack.herokuapp.com/"
+      />
+    </div>
+  </section>
+)
 
 export default ChatForm
