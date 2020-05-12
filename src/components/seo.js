ca6e65e26bc0a4f42a69cb8f6d75277ae48c82a9
Cuong Vu <vuhuucuong1310@gmail.com>
2020-05-12 16:11:43 +0700

    edit SEO func

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,11 +3,15 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 function SEO({
-  description: descriptionProps,
+  title: titleProps = 'WeBuild Community',
+  description: descriptionProps = 'WeBuild Community is a platform that connects developers in Vietnam, where they can share their knowledge and experience, while working, learning, and building cool stuffs together.',
   lang = 'en',
   meta = [],
-  keywords = [],
-  title: titleProps,
+  keywords = ['WeBuild', 'commutity', 'connect', 'developer', 'Vietnam'],
+  path = '',
+  ogImagePath = '../assets/images/webuild-og.png',
+  fbAppId = '',
+  fbPages = '',
 }) {
   const { site } = useStaticQuery(graphql`
     query DefaultSEOQuery {
@@ -50,15 +54,43 @@ function SEO({
         },
         {
           name: `og:url`,
-          content: `siteRoot`,
+          content: `${siteRoot}${path}`,
         },
         {
           name: `og:site_name`,
           content: siteName,
         },
         {
+          name: `og:image`,
+          content: ogImagePath,
+        },
+        {
+          name: `og:image:type`,
+          content: 'image/png',
+        },
+        {
+          name: `og:image:width`,
+          content: '1200',
+        },
+        {
+          name: `og:image:height`,
+          content: '680',
+        },
+        {
+          name: `og:image:alt`,
+          content: title,
+        },
+        {
           name: `og:locale`,
-          content: 'en_US',
+          content: 'en_GB',
+        },
+        {
+          name: `fb:app_id`,
+          content: fbAppId,
+        },
+        {
+          name: `fb:pages`,
+          content: fbPages,
         },
         {
           name: `ia:markup_url`,
