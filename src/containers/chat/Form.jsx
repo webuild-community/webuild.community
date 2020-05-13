7f68defce5ac4a817532c11f8d3933f6b627974a
zlatan <zlatan.pham@gmail.com>
2020-05-13 19:36:52 +0700

    feat: add loader for chat iframe

--- src/containers/chat/Form.jsx
+++ src/containers/chat/Form.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import './chat.css';
 
-const ChatForm = () => (
-  <section className="chat-section">
-    <div className="chat-box">
-      <iframe
-        scrolling="no"
-        className="chat-iframe"
-        src="https://chat.webuild.community/"
-      />
-    </div>
-  </section>
-);
+const ChatForm = () => {
+  const [loading, setLoading] = useState(true);
+  return (
+    <section className="chat-section">
+      <div className="chat-box flex">
+        <div
+          className={classnames('spinner transition-opacity duration-200', {
+            'opacity-0 invisible': !loading
+          })}
+        >
+          <div className="bounce"></div>
+        </div>
+        <iframe
+          scrolling="no"
+          className={classnames('chat-iframe transition-opacity duration-200', {
+            'opacity-0': loading
+          })}
+          onLoad={() => {
+            setLoading(false);
+          }}
+          src="https://chat.webuild.community/"
+        />
+      </div>
+    </section>
+  );
+};
 
 export default ChatForm;
