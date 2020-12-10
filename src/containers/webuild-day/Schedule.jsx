077532c26e026f4836c34f8ee4b8643afd70fea2
Cuong Vu <vuhuucuong1310@gmail.com>
2020-12-10 18:08:54 +0700

    feat: update pics speakers

--- src/containers/webuild-day/Schedule.jsx
+++ src/containers/webuild-day/Schedule.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import Container from 'components/Container';
 import classnames from 'classnames';
 
+import hanNgoImg from 'assets/images/webuild-day/speakers/han-ngo.jpg';
+import thuyPhanImg from 'assets/images/webuild-day/speakers/thuy-phan.jpg';
+import sonDaoImg from 'assets/images/webuild-day/speakers/son-dao.jpg';
+import toanTranImg from 'assets/images/webuild-day/speakers/toan-tran.jpg';
+import phucLeImg from 'assets/images/webuild-day/speakers/phuc-le.jpeg';
+import nguyenAnImg from 'assets/images/webuild-day/speakers/nguyen-an.jpg';
+import huyNguyenImg from 'assets/images/webuild-day/speakers/huy.jpg';
+import andyHoImg from 'assets/images/webuild-day/speakers/andy-ho.jpg';
+import nguyenVuImg from 'assets/images/webuild-day/speakers/nguyen-vu.jpg';
+import hienTuImg from 'assets/images/webuild-day/speakers/hien-tu.jpg';
+import haiBienImg from 'assets/images/webuild-day/speakers/hai-bien.jpg';
+
 const listSchedule = [
   {
     time: '9:00 - 9:10',
@@ -10,7 +22,8 @@ const listSchedule = [
     at: '',
     tag: '#WeBuild',
     title: 'WeBuild Community in 2020',
-    description: ''
+    description: '',
+    pic: hanNgoImg
   },
   {
     time: '9:20 - 9:50',
@@ -19,7 +32,8 @@ const listSchedule = [
     at: '@Line',
     tag: '#Android',
     title: 'Dependency injection with Hilt',
-    description: ''
+    description: '',
+    pic: thuyPhanImg
   },
   {
     time: '10:00 - 10:30',
@@ -28,7 +42,8 @@ const listSchedule = [
     at: '@Carp Tech',
     tag: '#Flutter',
     title: 'Building a beautiful Flutter app',
-    description: ''
+    description: '',
+    pic: sonDaoImg
   },
   {
     time: '10:40 - 11:10',
@@ -37,7 +52,8 @@ const listSchedule = [
     at: '@Lazada',
     tag: '#Android',
     title: 'Reactive Android Programming',
-    description: ''
+    description: '',
+    pic: toanTranImg
   },
   {
     time: '11:20 - 11:50',
@@ -46,7 +62,8 @@ const listSchedule = [
     at: '@Dwarves, Swift Vietnam',
     tag: '#Swift',
     title: 'XPC service / iOS',
-    description: ''
+    description: '',
+    pic: phucLeImg
   },
   {
     time: '',
@@ -64,7 +81,8 @@ const listSchedule = [
     at: '@Prime Data',
     tag: '#Elixir',
     title: 'DDD - Bible for Software Development at scale',
-    description: ''
+    description: '',
+    pic: nguyenAnImg
   },
   {
     time: '14:10 - 14:40',
@@ -73,7 +91,8 @@ const listSchedule = [
     at: '@Holistics, Grokking',
     tag: '#Grokking',
     title: 'Building and running DBML as an open-source project',
-    description: ''
+    description: '',
+    pic: huyNguyenImg
   },
   {
     time: '14:50 - 15:20',
@@ -82,7 +101,8 @@ const listSchedule = [
     at: '@Axie Infinity, Rust Vietnam',
     tag: '#Rust',
     title: 'Rust Actor model in game development',
-    description: ''
+    description: '',
+    pic: andyHoImg
   },
   {
     time: '15:30 - 15:45',
@@ -100,7 +120,8 @@ const listSchedule = [
     at: '',
     tag: '#Golang',
     title: 'Deploy Kubernetes on premise',
-    description: ''
+    description: '',
+    pic: nguyenVuImg
   },
   {
     time: '16:35 - 17:05',
@@ -109,7 +130,8 @@ const listSchedule = [
     at: '@beGroup, Cloud Native',
     tag: '#Cloud Native',
     title: 'Golang monorepo in the wild. A pathway to continuous deployment',
-    description: ''
+    description: '',
+    pic: hienTuImg
   },
   {
     time: '17:15 - 17:45',
@@ -118,7 +140,8 @@ const listSchedule = [
     at: '@Golang Vietnam',
     tag: '#Golang',
     title: 'Goroutine Underlying',
-    description: ''
+    description: '',
+    pic: haiBienImg
   },
   {
     time: '18:00',
@@ -142,7 +165,7 @@ const Schedule = () => {
           </h2>
           {listSchedule.map(
             (
-              { time, name, position, at, tag, title, description },
+              { time, name, position, at, tag, title, description, pic },
               index,
               arr
             ) => {
@@ -164,6 +187,14 @@ const Schedule = () => {
                     {time}
                   </div>
                   <div className="flex w-3/5 md:ml-8">
+                    {pic && (
+                      <img
+                        className="w-12 h-12 mr-4 rounded-full object-cover"
+                        style={{ filter: 'grayscale(100%)' }}
+                        src={pic}
+                        alt={name}
+                      />
+                    )}
                     <div>
                       <p className="font-bold">{name}</p>
                       <p className="text-white">{position}</p>
