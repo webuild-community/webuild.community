a9885e3e9ac724f6e72549068190daa92236c90a
Thanh Pham <zlatan.pham@gmail.com>
2021-01-13 19:53:08 +0700

    feat: add tweet list section (#37)
    Co-authored-by: thuutri <thuutri2710@gmail.com>

---
+++ containers/home/Twitter.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import Container from 'components/Container';
+
+const Skeleton = () => (
+  <div className="p-10 space-y-10">
+    <div className="h-4 w-16 bg-gray-100" />
+    <div className="space-y-8">
+      <div className="space-y-3">
+        <div className="h-4 w-full bg-gray-100" />
+        <div className="h-4 w-2/3 bg-gray-100" />
+      </div>
+      <div className="space-y-3">
+        <div className="h-4 w-3/5 bg-gray-100" />
+        <div className="h-4 w-1/3 bg-gray-100" />
+      </div>
+      <div className="space-y-3">
+        <div className="h-4 w-full bg-gray-100" />
+        <div className="h-4 w-2/3 bg-gray-100" />
+      </div>
+    </div>
+  </div>
+);
+
+const TwitterSection = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    document.getElementsByClassName('twitter-embed')[0].appendChild(script);
+  }, []);
+
+  return (
+    <section id="twitter" className="pb-16 pt-6 sm:pb-24 lg:pt-2 bg-white">
+      <Container>
+        <div className="twitter-embed bg-white border-gray-200 border overflow-hidden max-w-xl mx-auto">
+          {/* below html servers as placeholder, they will be replaced once Twitter's script loaded */}
+          <a
+            className="twitter-timeline"
+            data-height={600}
+            href="https://twitter.com/nntruonghan/lists/webuild-community-19282?ref_src=twsrc%5Etfw"
+          >
+            <Skeleton />
+          </a>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default TwitterSection;
