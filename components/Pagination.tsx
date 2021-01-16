5ccde9c82aa46d3b72a4cf69458c91884242a2cb
thuutri <thuutri2710@gmail.com>
2021-01-16 23:17:32 +0700

    chore: change after review

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface PaginationProps {
   total: number;
@@ -9,13 +10,15 @@ interface PaginationProps {
 
 const Pagination = ({ total, current, pageSize }: PaginationProps) => {
   const numberOfPage = Math.round(total / pageSize);
+  const router = useRouter();
+  const currentPath = router.asPath.split('?')[0];
 
   return (
     <nav>
       <ul className="list-reset flex justify-center mt-6">
         {current > 1 && (
           <li key="first" className="inline-block flex-none mx-1">
-            <Link href="?page=1">
+            <Link href={`${currentPath}?page=1`}>
               <a className="no-underline text-primary hover:text-black">
                 « First
               </a>
@@ -24,7 +27,7 @@ const Pagination = ({ total, current, pageSize }: PaginationProps) => {
         )}
         {current > 1 && (
           <li key="prev" className="inline-block flex-none mx-1">
-            <Link href={`?page=${current - 1}`}>
+            <Link href={`${currentPath}?page=${current - 1}`}>
               <a className="no-underline text-primary hover:text-black">
                 ‹ Prev
               </a>
@@ -34,7 +37,7 @@ const Pagination = ({ total, current, pageSize }: PaginationProps) => {
         {[...Array(numberOfPage)].map((_, i) => (
           <li key={i} className="inline-block flex-none mx-1">
             {i + 1 !== current ? (
-              <Link href={`?page=${i + 1}`}>
+              <Link href={`${currentPath}?page=${i + 1}`}>
                 <a className="no-underline text-primary hover:text-black">
                   {i + 1}
                 </a>
@@ -46,7 +49,7 @@ const Pagination = ({ total, current, pageSize }: PaginationProps) => {
         ))}
         {current < numberOfPage && (
           <li key="next" className="inline-block flex-none mx-1">
-            <Link href={`?page=${current + 1}`}>
+            <Link href={`${currentPath}?page=${current + 1}`}>
               <a className="no-underline text-primary hover:text-black">
                 Next ›
               </a>
@@ -55,7 +58,7 @@ const Pagination = ({ total, current, pageSize }: PaginationProps) => {
         )}
         {current < numberOfPage && (
           <li key="last" className="inline-block flex-none mx-1">
-            <Link href={`?page=${total}`}>
+            <Link href={`${currentPath}?page=${total}`}>
               <a className="no-underline text-primary hover:text-black">
                 Last »
               </a>
