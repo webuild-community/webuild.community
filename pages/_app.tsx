70ed62ac1b74152396311da2221836b62489b722
zlatan <zlatan.pham@gmail.com>
2021-02-09 18:38:46 +0700

    fix: wrong og url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,27 @@ import React from 'react';
 import '../styles/main.css';
 import App from 'next/app';
 import { DefaultSeo } from 'next-seo';
+import { useRouter } from 'next/router';
+import { ORIGIN } from 'constants/url';
+
+const SEORender = () => {
+  const { asPath } = useRouter();
+  return (
+    <DefaultSeo
+      title="WeBuild Community - Vietnam Alliance hub of Developers & Makers"
+      openGraph={{
+        type: 'website',
+        locale: 'vi_VN',
+        description:
+          'WeBuild Community is a platform that connects developers in Vietnam, where they can share their knowledge and experience, while working, learning, and building cool stuffs together.',
+        url: `${ORIGIN}${asPath}`,
+        site_name: 'WeBuild Community',
+        title: 'WeBuild Community'
+      }}
+      description="WeBuild Community is a platform that connects developers in Vietnam, where they can share their knowledge and experience, while working, learning, and building cool stuffs together."
+    />
+  );
+};
 
 class MyApp extends App {
   render() {
@@ -9,19 +30,7 @@ class MyApp extends App {
 
     return (
       <>
-        <DefaultSeo
-          title="WeBuild Community - Vietnam Alliance hub of Developers & Makers"
-          openGraph={{
-            type: 'website',
-            locale: 'vi_VN',
-            description:
-              'WeBuild Community is a platform that connects developers in Vietnam, where they can share their knowledge and experience, while working, learning, and building cool stuffs together.',
-            url: `https://webuild.community`,
-            site_name: 'WeBuild Community',
-            title: 'WeBuild Community'
-          }}
-          description="WeBuild Community is a platform that connects developers in Vietnam, where they can share their knowledge and experience, while working, learning, and building cool stuffs together."
-        />
+        <SEORender />
         <main>
           <Component {...pageProps} />
         </main>
