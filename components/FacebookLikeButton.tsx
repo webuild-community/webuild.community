60ed21e67819abb6fd0c971022c7b8d2fb25b97a
zlatan <zlatan.pham@gmail.com>
2021-02-18 11:33:41 +0700

    fix: fb like button not working for JS routing

--- components/FacebookLikeButton.tsx
+++ components/FacebookLikeButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo, useEffect } from 'react';
 import classnames from 'classnames';
 
 interface Props {
@@ -12,37 +12,41 @@ interface Props {
 
 const fbAppId = process.env.FACEBOOK_APP_ID || '';
 
-function FacebookLikeButton({
-  href,
-  size = 'small',
-  layout = 'button_count',
-  share = false,
-  appId = fbAppId,
-  className
-}: Props) {
-  return (
-    <>
-      <div
-        className={classnames('fb-like', className)}
-        data-href={href}
-        data-width=""
-        data-size={size}
-        data-layout={layout}
-        data-action="like"
-        data-share={share.toString()}
-      />
+const FacebookLikeButton = memo(
+  ({
+    href,
+    size = 'small',
+    layout = 'button_count',
+    share = false,
+    appId = fbAppId,
+    className
+  }: Props) => {
+    useEffect(() => {
+      const script = document.createElement('script');
+      script.crossOrigin = 'anonymous';
+      script.src = `https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v7.0&appId=${appId}`;
+      script.async = true;
+      const fbRoot = document.getElementById('fb-root');
+      fbRoot?.appendChild(script);
+    });
 
+    return (
       <>
-        <div id="fb-root" />
-        <script
-          async
-          defer
-          crossOrigin="anonymous"
-          src={`https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v7.0&appId=${appId}`}
+        <div
+          className={classnames('fb-like', className)}
+          data-href={href}
+          data-width=""
+          data-size={size}
+          data-layout={layout}
+          data-action="like"
+          data-share={share.toString()}
         />
+
+        <div id="fb-root" />
       </>
-    </>
-  );
-}
+    );
+  },
+  () => false
+);
 
 export default FacebookLikeButton;
