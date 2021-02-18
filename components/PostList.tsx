526a036471852158b0aeb78ae50751a4a413aec9
thuutri <thuutri2710@gmail.com>
2021-02-18 09:06:13 +0700

    fix: fb-like-button is hidden

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BlogPostResponse } from '../apis/contentful';
-import Link from 'next/link';
 import Pagination from 'components/Pagination';
 import PostInfo from './PostInfo';
 
@@ -12,11 +11,12 @@ const PostList = ({ entries, total, skip, limit }: BlogListProps) => {
       <div className="divide-y divide-gray-200">
         {entries.map(({ slug, title, createdAt, tags }) => (
           <div className="py-3" key={slug}>
-            <Link href={`/news/${slug}`}>
-              <a className="text-lg font-bold mb-1 hover:text-primary transition-all duration-200">
-                {title}
-              </a>
-            </Link>
+            <a
+              href={`/news/${slug}`}
+              className="text-lg font-bold mb-1 hover:text-primary transition-all duration-200"
+            >
+              {title}
+            </a>
 
             <PostInfo createdAt={createdAt} tags={tags} />
           </div>
