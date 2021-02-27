ef13bef82bb75d533a4946a8f80b45268f964123
zlatan <zlatan.pham@gmail.com>
2021-02-27 23:40:56 +0700

    fix: add link to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -67,6 +67,19 @@ const Header = ({ variant = 'default' }) => {
                 </a>
               </Link>
             </li>
+            <li className="px-6">
+              <Link href="/code-for-vietnam">
+                <a
+                  className={classnames({
+                    'text-subprimary': isPrimaryVariant,
+                    'text-gray-700': !isPrimaryVariant,
+                    'font-bold': pathname.startsWith('/code-for-vietnam')
+                  })}
+                >
+                  Code for Vietnam
+                </a>
+              </Link>
+            </li>
           </ul>
 
           <a href="https://we-build-vn.slack.com/join/shared_invite/zt-gh7pb9o1-eUEruSdfycMEkKfk5Bkdww#/">
@@ -127,6 +140,19 @@ const Header = ({ variant = 'default' }) => {
                 })}
               >
                 News
+              </a>
+            </Link>
+          </li>
+          <li className="pr-8">
+            <Link href="/news">
+              <a
+                className={classnames({
+                  'text-subprimary': isPrimaryVariant,
+                  'text-gray-700': !isPrimaryVariant,
+                  'font-bold': pathname.startsWith('/code-for-vietnam')
+                })}
+              >
+                Code for Vietnam
               </a>
             </Link>
           </li>
